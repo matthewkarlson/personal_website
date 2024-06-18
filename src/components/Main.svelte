f5dc09c87a1248a3c0cf06e07d079db50ac7686c
Matthew Karlson <mattjkarlson@gmail.com>
2024-06-18 15:54:06 +0100

    updated all text fields

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -3,20 +3,20 @@
 
     let steps = [
         {
-            name: "Smoljames Store",
-            icon: "fa-solid fa-cart-shopping",
+            name: "Sensors in Autonomous Vehicles",
+            icon: "fa-solid fa-car",
             description:
-                "Smoljames Store is a a merchanising store created with Next.js, Commerce.js & Stripe! Commerce.js is a CMS for managing project and Stripe is used for all transaction handling.",
+                "Worked with ON Semiconductors to test a pre-production image sensor for applications in autonomous vehicles. Developed a custom test suite to evaluate the sensor's performance in various lighting conditions and at different speeds. The test suite was used to evaluate the sensor's performance and identify potential areas for improvement.",
         },
         {
-            name: "Utlimate Todos",
-            icon: "fa-solid fa-list-check",
+            name: "House Price Prediction Using ML",
+            icon: "fa-solid fa-house",
             description:
                 "Test for model robustness with customized test profiles and receive exhaustive reporting on the performance and potential vulnerabilities of your model.",
         },
         {
-            name: "Pokédex",
-            icon: "fa-solid fa-diagram-project",
+            name: "Data Visualization & Augmentation",
+            icon: "fa-solid fa-chart-line",
             description:
                 "Augment your datasets with our filters and dataset manipulations to ensure your models are trained on the highest quality datasets (coming soon).",
         },
@@ -25,19 +25,20 @@
     let benefits = [
         {
             metric: "10x",
-            name: "a self taught developer",
+            name: "A Rational Optimist",
             description:
-                "I taught myself to code using free online resources and absolutely fell in love with the creativity and problem solving that is involved in developing and engineering innovative new online experiences. Starting of with JavaScript, HTML & CSS and evolving my knowledge base to include JavaScript frameworks, backend programming, design, cloud services and much much more.",
+                "We live in the best time in human history. We have more wealth, more freedom, and more opportunities than ever before, despite what the news might tell you. I believe we have a responsibility to use our skills and resources to make the world an even better place by building things that add value to the world.",
+},
+        {
+            name: "A Writer",
+            description:
+            "I write about technology, wealth creation, optimism and my general thoughts. I believe that writing is a powerful tool for learning and growth. I write to clarify my thoughts and to share my ideas with the world. Sharing my thoughts publicly keeps me accountable and allows me to get rapid feedback on ideas and thougts.",
         },
         {
-            name: "a product design & UX finatic",
+            metric: "10x",
+            name: "A Stoic",
             description:
-                "Carefully crafting and designing amazing user experiences allows me to express and experiment with every morsel of creativity I have. I love the challenge of learning new design concepts and enabling users with amazing online experiences.",
-        },
-        {
-            name: "an excellent communicator",
-            description:
-                "Communication is key and it's a paramount value of mine. I believe in transparency and constructive communication above all else. This helps me develop deep relationships and ensures my effectiveness and productivity in any work space with any team.",
+            "Stoicism is at the core of my personal philisophy. I believe in focusing on the things that we can control and letting go of the rest. There are a lot of things we can't control, and thats ok. By remaining focused we waste less time and live a more fulfilling and peaceful life. Our minds and our actions are the only things we have true control over, so my life is based around a ruthless focus on how I can think and act in a way that is most beneficial to myself and others."
         },
     ];
 </script>
@@ -53,13 +54,11 @@
             <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
                 Hi! I'm <span class="poppins text-violet-400">Matt</span>
                 Karlson
-                <br /> <span class="poppins text-violet-400">Full Stack</span>
-                and <span class="poppins text-violet-400">Blockchain</span> Developer
+                <br /> <span class="poppins text-violet-400">Machine Learning</span>
+                and <span class="poppins text-violet-400">AI</span> Engineer
             </h2>
             <p class="text-base sm:text-lg md:text-xl">
-                My <span class="text-violet-400"> favorite tech</span> includes JavaScript
-                (NEXT.JS or SvelteKit), TailwindCSS, Node.js + Express.js & PostgreSQL
-                or Firebase/Firestore!
+                My <span class="text-violet-400"> favorite tech</span> includes Python, Streamlit, Javascript, and Solidity. I'm passionate about building things that add value to the world.
             </p>
             <button
                 class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
@@ -68,8 +67,7 @@
                     class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
                 />
                 <a
-                    href="https://www.linkedin.com/in/matthew-karlson-a69b031a3/"
-                    target="_blank"
+                    href="mailto:hello@mattkarlson.com"
                 >
                     <h4 class="relative z-9">Get in touch &rarr;</h4></a
                 >
@@ -86,52 +84,24 @@
     <section class="py-20 lg:py-32 flex flex-col gap-24" id="projects">
         <div class="flex flex-col gap-2 text-center">
             <h6 class="text-large sm:text-xl md:text-2xl">
-                A few of my creative endeavors.
+                A few of my recent projects
             </h6>
-            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
-                Curious to <span class="poppins text-violet-400">see</span> my work?
-            </h3>
         </div>
-        <a
-            href=""
-            target="_blank"
-            class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
-        >
-            <i class="fa-regular fa-circle-play" />
-            <p>Watch the video</p>
-        </a>
+
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
             <Step step={steps[0]}>
                 <p>
-                    Smoljames Store is a a merchanising store created with <strong
-                        class="text-violet-400"
-                        >Next.js, Commerce.js, Stripe & Node.js + Express.js!</strong
-                    > Commerce.js is a product CMS and Stripe is used for all transaction
-                    handling.
+                    Worked with <strong class="text-violet-400">ON Semiconductor</strong> to test a pre-production image sensor for applications in autonomous vehicles. Developed a <strong class="text-violet-400">custom test suite </strong>to evaluate the sensor's performance in various lighting conditions and at different speeds. Experimented with Convolutional filters to improve sensor performance.
                 </p>
             </Step>
             <Step step={steps[1]}>
                 <p>
-                    Ultimate Todos is a Full Stack <strong
-                        class="text-violet-400">Next.js</strong
-                    >,
-                    <strong class="text-violet-400">Node.js + Express.js</strong
-                    >
-                    & <strong class="text-violet-400">Firebase</strong> CRUD application
-                    that allows a user to login, manage a tidy and efficacious todo
-                    list, and persist this information across devices.
+                    Built a basic machine learning model to predict house prices using the <strong class="text-violet-400">HMRC Housing</strong>. The model was trained using a <strong class="text-violet-400">Random Forest Regressor</strong> and evaluated using <strong class="text-violet-400">cross-validation</strong>. The model was then deployed using <strong class="text-violet-400">Streamlit</strong> and <strong class="text-violet-400">Heroku</strong>.
                 </p>
             </Step>
             <Step step={steps[2]}>
                 <p>
-                    The Pokédex is a <strong class="text-violet-400"
-                        >SvelteKit & TailwindCSS</strong
-                    >
-                    web application, hosted on
-                    <strong class="text-violet-400">Netlify</strong>, that
-                    consumes and caches the
-                    <strong class="text-violet-400">Pokémon API</strong>
-                    to display all Pokémon information. Gotta catch them all!
+                    Built out multiple data dashboards as <strong class="text-violet-400" >Streamlit apps</strong> and deployed them as web apps using a <strong class="text-violet-400" >Kubernetes</strong> cluster. I engineered <strong class="text-violet-400" >SQL Queries</strong> on the backend, built out the logic in python and designed the frontend.
                 </p>
             </Step>
         </div>
@@ -187,71 +157,19 @@
             {/each}
         </div>
         <h5 class={" text-2xl sm:text-3xl font-semibold text-center poppins "}>
-            The <span class="text-violet-400">Complete</span> Package
+            If these things interest <span class="text-violet-400">you</span>, I would <span class="text-violet-400">love</span> to hear from you. Check out all my links <span class="text-violet-400">below.</span> 
         </h5>
+
         <div
             class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full"
         >
-            <table class="bg-white text-slate-700 rounded text-center">
-                <thead class={"border-b border-solid border-slate-200  "}>
-                    <tr class="">
-                        <th />
-                        <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
-                        <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
-                        <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
-                        <th
-                            class="bg-violet-700 text-white whitespace-nowrap p-4 px-8"
-                            >Me</th
-                        >
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="border-b border-solid border-slate-200">
-                        <td
-                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
-                            >Dedication</td
-                        >
-                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
-                        <td><i class="fa-solid fa-check text-slate-500" /></td>
-                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
-                        <td><i class="fa-solid fa-check text-green-500" /></td>
-                    </tr>
-                    <tr class="border-b border-solid border-slate-200">
-                        <td
-                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
-                            >Critical Thought</td
-                        >
-                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
-                        <td><i class="fa-solid fa-check text-slate-500" /></td>
-                        <td><i class="fa-solid fa-check text-slate-500" /></td>
-                        <td><i class="fa-solid fa-check text-green-500" /></td>
-                    </tr>
-                    <tr>
-                        <td
-                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
-                            >Interpersonal Skills</td
-                        >
-                        <td><i class="fa-solid fa-check text-slate-500" /></td>
-                        <td><i class="fa-solid fa-check text-slate-500" /></td>
-                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
-                        <td><i class="fa-solid fa-check text-green-500" /></td>
-                    </tr>
-                    <tr class="border-t border-solid border-slate-200">
-                        <td
-                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
-                            >Progamming Ability</td
-                        >
-                        <td><i class="fa-solid fa-check text-slate-500" /></td>
-                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
-                        <td><i class="fa-solid fa-check text-slate-500" /></td>
-                        <td><i class="fa-solid fa-check text-green-500" /></td>
-                    </tr>
-                </tbody>
-            </table>
+
+        <script async data-uid="c7ad401ca8" src="https://matthew-karlson.ck.page/c7ad401ca8/index.js"></script>
+
         </div>
         <div class="mx-auto -mt-12 italic sm:hidden opacity-50">
             <p>Scroll to see more &rarr;</p>
         </div>
-        <p class="mx-auto">So why not invest?</p>
+
     </section>
 </main>
