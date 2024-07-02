7bcd82d5075317e2160d7a12377ec6fcef445865
Matthew Karlson <mattjkarlson@gmail.com>
2024-07-02 08:19:12 +0100

    minor updates

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,17 +1,29 @@
 <footer
     class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
 >
-
-    <p class="px-4 py-2 bg-white text-slate-950 font-medium" id ="contact">
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium" id="contact">
         Connect with me &darr;
     </p>
     <div class="flex flex-col gap-4 items-center justify-center w-full">
         <p>
             <b class="pr-2">Email</b>
-            <a
-                href="mailto:hello@mattkarlson.com"
-                class="text-violet-400"
+            <a href="mailto:hello@mattkarlson.com" class="text-violet-400"
                 >hello@mattkarlson.com<sup class=""
+                    ><span class="text-xs scale-75 pl-0.5"
+                        ><i
+                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                        /></span
+                    ></sup
+                ></a
+            >
+        </p>
+        <p>
+            <b class="pr-2">X/Twitter</b>
+            <a
+                href="https://x.com/matthew_karlson"
+                target="_blank"
+                class="text-violet-400"
+                >matthew_karlson<sup class=""
                     ><span class="text-xs scale-75 pl-0.5"
                         ><i
                             class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
@@ -51,7 +63,25 @@
             >
         </p>
     </div>
-    <div class = "px-10">
-        <p>This website was built using Svelte and Tailwind following <a class="text-violet-400" href="https://www.youtube.com/watch?v=-2UjwQzxvBQ&t=3449s" target="_blank">this</a> tutorial. It's hosted on Heroku and I set up the custom domain through Cloudflare. The particle effect is from <a href="https://github.com/VincentGarreau/particles.js/" target="_blank" class="text-violet-400">particle.js</a>. The code for this website can be found <a class="text-violet-400" href="https://github.com/matthewkarlson/personal_website" target="_blank">here</a></p>
+    <div class="px-10">
+        <p>
+            This website was built using Svelte and Tailwind following <a
+                class="text-violet-400"
+                href="https://www.youtube.com/watch?v=-2UjwQzxvBQ&t=3449s"
+                target="_blank">this</a
+            >
+            tutorial. It's hosted on Heroku and I set up the custom domain
+            through Cloudflare. The particle effect is from
+            <a
+                href="https://github.com/VincentGarreau/particles.js/"
+                target="_blank"
+                class="text-violet-400">particle.js</a
+            >. The code for this website can be found
+            <a
+                class="text-violet-400"
+                href="https://github.com/matthewkarlson/personal_website"
+                target="_blank">here</a
+            >
+        </p>
     </div>
 </footer>
