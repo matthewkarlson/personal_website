12b9b30bf31795ce73d1bb54064022fd194c3fdc
Matthew Karlson <mattjkarlson@gmail.com>
2024-07-03 08:02:28 +0100

    converting back to heroku

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,20 @@
-// import adapter from "@sveltejs/adapter-node";
-// import  vitePreprocess  from  'svelte-preprocess';
+import adapter from "@sveltejs/adapter-node";
+import  vitePreprocess  from  'svelte-preprocess';
 
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-//   preprocess: vitePreprocess(),
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
 
-//   kit: {
-
-//         adapter: adapter({
-//           pages: 'build',
-//           assets: 'build',
-//           fallback: null
-//         }),
-//         // Other SvelteKit configurations
-//       }
-
-// };
-
-// export default config;
-import vercel from '@sveltejs/adapter-vercel';
-
-export default {
   kit: {
-    adapter: vercel(),
-    // Other SvelteKit configurations
-  }
+
+        adapter: adapter({
+          pages: 'build',
+          assets: 'build',
+          fallback: null
+        }),
+        // Other SvelteKit configurations
+      }
+
 };
+
+export default config;
